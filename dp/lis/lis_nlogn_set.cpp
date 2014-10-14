55483bd6162ef76ba22882d34d9a039000645d22
2dev2 <2dev2@users.noreply.github.com>
2014-10-14 16:47:06 +0530

    Update lis_nlogn_set.cpp

--- dp/lis/lis_nlogn_set.cpp
+++ dp/lis/lis_nlogn_set.cpp
@@ -1,5 +1,10 @@
 //We will see here an algorithm which take O(n log k) time where k is the size of the LIS.
 //http://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
+/*
+Only they use set instead of array. This helps in finding a place
+to update. With array you should implement binary search.
+There is a mistake though. It should be multiset, not set.
+*/
 #include<iostream>
 #include<set>
 #include<vector>
