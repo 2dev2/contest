199819231b15ce0e3e11ab728b0aacba36d0ca94
2dev2 <2dev2@users.noreply.github.com>
2014-10-15 02:14:45 +0530

    Create permutation.cpp

---
+++ backtracking/permutation.cpp
@@ -0,0 +1,26 @@
+char arr[]={'a','b','c','d'};
+vector<char>result;
+int taken[20]={0};
+void call()
+{
+	if(result.size()==4)
+	{
+		for(int i=0;i<4;i++)
+		printf("%c",result[i]);
+		puts("");
+		return;
+	}
+	for(int i=0;i<4;i++)
+	{
+		if(taken[i]==0)
+		{
+			taken[i]=1; result.push_back(arr[i]);
+			call();
+			taken[i]=0; result.pop_back();
+		}
+	}
+}
+int main()
+{
+	call();
+}
